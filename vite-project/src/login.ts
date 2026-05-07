eeb6b7bfc93041bbdfa950ecbf0d648b934d485c
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2026-05-06 21:18:51 -0300

    Adicionando função de login conectado a api

--- vite-project/src/login.ts
+++ vite-project/src/login.ts
@@ -1,4 +1,5 @@
-const form = document.getElementById("loginForm") as HTMLFormElement;
+const form = document.getElementById("login-form") as HTMLFormElement;
+const API_URL = import.meta.env.VITE_API_URL;
 
 form.addEventListener("submit", async (e: Event) => {
   e.preventDefault();
@@ -6,7 +7,7 @@ form.addEventListener("submit", async (e: Event) => {
   const password = (document.getElementById("password") as HTMLInputElement)
     .value;
   try {
-    const response = await fetch("http://localhost:3000/api/login", {
+    const response = await fetch(`${API_URL}/api/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,1 +1,20 @@
+const API_URL = import.meta.env.VITE_API_URL;
 
+const token = localStorage.getItem('@token')
+const userStorage = JSON.parse(localStorage.getItem('@user'));
+const user = userStorage ? userStorage : null;
+
+const navArea =  document.getElementById('nav-area')!;
+const postContainer =  document.getElementById('post-container')!;
+
+const setupHeader = () =>{
+  if (token && user) {
+    navArea.innerHTML =/*html*/` <span class="mr-4 text-gray-600">Olá<strong>${user.name}</strong></span>
+    <button id="btn-logout" class= "text-red-500 hover:text-red-600 hover:underline font-bold">Sair</button>`;
+
+  } else {
+    navArea.innerHTML =/*html*/` <a href="/login.html" class="bg-blue900 hover: bg-blue800 text-white px4 py-2 rounded">Login</a>
+    <p></p> <h1></h1>`;
+  }
+};
+setupHeader();
